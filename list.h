04346f47b0323bd0275599957bc284ed20ead027
bredenhulse <91069868+bredenhulse@users.noreply.github.com>
2021-11-02 08:25:09 -0500

    Add files via upload

---
+++ list.h
@@ -0,0 +1,97 @@
+struct Node{
+    char val;
+    Node* next;
+
+    Node(){
+        this->val = 0;
+        this->next = nullptr;
+    }
+    Node(char v){
+        this->val = v;
+        this->next = nullptr;
+    }
+    Node(char v, Node n){
+        this->val = v;
+        this->next = &n;
+    }
+};
+
+class List{
+    private:
+        Node* front; //points to the front of list
+        Node* it; //points to the current node
+        int size; // gives size
+        bool empty; //checks to see if empty
+
+    public:
+        List(){
+            this->front = new Node('[');
+            this->it = front;
+            this->size = 0;
+        }
+        List(char v){
+            Node n(v);
+            this->front = &n;
+            this->it = &n;
+            this->size = 1;
+        }
+        char get_iterator(){
+            if(it == nullptr){
+                return ']';
+            }
+            else{
+                return it->val; // Will return the value where the iterator is
+            }
+        }
+        void begin(){
+            it = front; // will move iterator to the beginning
+        }
+        void end(){
+            while(it->next != nullptr){            
+                it = it->next; // will move iterator to the end
+            }
+        }
+        bool isEmpty(){ // Check to see if empty
+            if(front == nullptr){
+                empty = true;
+            }
+            else{
+                empty = false;
+            }
+            return empty;
+        }
+        int getfront(){ //Get front value
+            return front -> val;
+        }
+        void move_up(){ //move iterator forward one
+            it = it->next;
+        }
+        void push_front(char x){ //Add another value to the front
+            Node temp(x,*front);
+            front = &temp;
+            size++;
+        }
+        void pop_front(){ // remove the front
+            Node *temp = front;
+            front = front->next;
+            delete temp;
+        }
+        void insert_after(char y){ //insert value where desired
+            if(it->next != nullptr){
+                Node *temp = new Node(y);
+                temp->next = it->next;
+                it->next = temp;
+                it = temp;
+            }
+            else{
+                Node* temp = new Node(y);
+                it->next = temp;
+                it = temp;
+            }
+            size++;
+        }
+        void clear(){ //Will empty the list
+            Node *temp = front;
+            it = front;
+        }
+};
---
+++ main.cpp
@@ -0,0 +1,47 @@
+/*
+Name: Breden Hulse
+Class: Computer Science 2
+Description: It is a linked list.
+*/
+#include <iostream>
+#include "list.h"
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    List beiju;
+    //open files
+    ifstream infile;
+    infile.open("Test.txt");
+    ofstream fout;
+    fout.open("outfile");
+    string testCase;
+    //reads in the file
+    while(infile >> testCase){
+        beiju.begin();
+        for(int i = 0; i < testCase.size(); i++){
+            if(testCase[i] == '['){
+               beiju.begin(); 
+            }
+            else if(testCase[i] == ']'){
+                beiju.end();
+            }
+            else{
+                beiju.insert_after(testCase[i]);
+            }
+        }
+        //Sends to the outfile
+        beiju.begin();
+        while(beiju.get_iterator() != ']'){
+            if(beiju.get_iterator() != '['){
+                fout << beiju.get_iterator();
+            }
+            beiju.move_up();
+        }
+        beiju.clear();
+        fout << endl;
+    }
+    infile.close();
+    fout.close();
+}
